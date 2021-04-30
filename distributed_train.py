569a557ef509f823469fe7638cd6d09afb05d6a6
NCJ Charlie <me@ncj.wiki>
2021-04-30 11:50:59 +0800

    Update distributed_train.py

--- distributed_train.py
+++ distributed_train.py
@@ -1,35 +1,10 @@
-import sys
-import os
-import torch.distributed as dist
+from mpi4py import MPI
 
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
 
-def init_print(rank, size, debug_print=True):
-    if not debug_print:
-        """ In case run on hundreds of nodes, you may want to mute all the nodes except master """
-        if rank > 0:
-            sys.stdout = open(os.devnull, 'w')
-            sys.stderr = open(os.devnull, 'w')
-    else:
-        # labelled print with info of [rank/size]
-        old_out = sys.stdout
-
-        class LabeledStdout:
-            def __init__(self, rank, size):
-                self._r = rank
-                self._s = size
-                self.flush = sys.stdout.flush
-
-            def write(self, x):
-                if x == '\n':
-                    old_out.write(x)
-                else:
-                    old_out.write('[%d/%d] %s' % (self._r, self._s, x))
-
-        sys.stdout = LabeledStdout(rank, size)
-
-
-if __name__ == "__main__":
-    dist.init_process_group(backend='mpi')
-    size = dist.get_world_size()
-    rank = dist.get_rank()
-    init_print(rank, size)
+if rank == 0:
+    data = {'a': 7, 'b': 3.14}
+    comm.send(data, dest=1, tag=11)
+elif rank == 1:
+    data = comm.recv(source=0, tag=11)
